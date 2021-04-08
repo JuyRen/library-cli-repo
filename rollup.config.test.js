3979ccfcf3edb21bddaa6278965bfb3b0568bb7d
renjunyang <renjunyang@auraxy.com>
2021-04-08 18:14:18 +0800

    chore: script 添加test

--- rollup.config.test.js
+++ rollup.config.test.js
@@ -1,15 +1,13 @@
 import { merge } from 'webpack-merge';
-import { terser } from 'rollup-plugin-terser';
 import baseConfig from './rollup.config.base';
 import pkg from './package.json';
 
 const config = merge(baseConfig, {
     output: {
         file: './lib/index.js',
-        format: 'es',
-        exports: 'auto'
+        format: 'cjs',
+        exports: 'default'
     },
-    plugins: [terser()],
     // 当format: cjs | es的时候，声明@babel/runtime系列包为external, 采用import 引入corejs提供的helper函数
     external: Object.keys(pkg.dependencies).map(item => new RegExp(item)) // 不打包指定的依赖包， 可以使用String | String[] |  RegExp | (id) => boolean
 });
