dcb7041ac53c3fcac09f1956184bfaff6b50f9ea
renjunyang <juyren@163.com>
2021-06-22 13:43:11 +0800

    chore: 修改rollup配置

--- rollup.config.node.js
+++ rollup.config.node.js
@@ -1,12 +1,14 @@
 import { merge } from 'webpack-merge';
 import baseConfig from './rollup.config.base';
+import pkg from './package.json';
 
 const config = merge(baseConfig, {
     output: {
         file: './lib/index.js',
         format: 'cjs',
         exports: 'default'
-    }
+    },
+    external: Object.keys(pkg.dependencies).map(item => new RegExp(item)) // 不打包指定的依赖包， 可以使用String | String[] |  RegExp | (id) => boolean
 });
 
 export default config;
--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,6 +1,7 @@
 import { merge } from 'webpack-merge';
 import { terser } from 'rollup-plugin-terser';
 import baseConfig from './rollup.config.base';
+import pkg from './package.json';
 
 const config = merge(baseConfig, {
     output: {
@@ -8,7 +9,8 @@ const config = merge(baseConfig, {
         format: 'es',
         exports: 'auto'
     },
-    plugins: [terser()]
+    plugins: [terser()],
+    external: Object.keys(pkg.dependencies).map(item => new RegExp(item)) // 不打包指定的依赖包， 可以使用String | String[] |  RegExp | (id) => boolean
 });
 
 export default config;
